82ad8ac1172b368f9d2b53a2d154fb15428fcbb6
Paulo Cesar <paulocgsantos99@gmail.com>
2024-12-30 16:12:18 -0300

    feat: add http controllers and services

--- src/main/java/com/paulotech/Desafio/backend/IFood/Api/controllers/CategoryController.java
+++ src/main/java/com/paulotech/Desafio/backend/IFood/Api/controllers/CategoryController.java
@@ -3,11 +3,11 @@ package com.paulotech.Desafio.backend.IFood.Api.controllers;
 import com.paulotech.Desafio.backend.IFood.Api.domain.categories.Category;
 import com.paulotech.Desafio.backend.IFood.Api.domain.categories.CategoryDTO;
 import com.paulotech.Desafio.backend.IFood.Api.services.CategoryService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -23,5 +23,23 @@ public class CategoryController {
     public ResponseEntity<Category> insert(@RequestBody CategoryDTO categoryData){
        Category newCategory = this.service.insert(categoryData);
        return ResponseEntity.ok().body(newCategory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll(){
+        List<Category> categories = this.service.getAll();
+        return ResponseEntity.ok().body(categories);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> update(@PathParam("id") String id, @RequestBody CategoryDTO categoryData){
+        Category updatedCategory = this.service.update(id, categoryData);
+        return ResponseEntity.ok().body(updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> delete(@PathParam("id") String id){
+        this.service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/paulotech/Desafio/backend/IFood/Api/services/CategoryService.java
+++ src/main/java/com/paulotech/Desafio/backend/IFood/Api/services/CategoryService.java
@@ -2,8 +2,11 @@ package com.paulotech.Desafio.backend.IFood.Api.services;
 
 import com.paulotech.Desafio.backend.IFood.Api.domain.categories.Category;
 import com.paulotech.Desafio.backend.IFood.Api.domain.categories.CategoryDTO;
+import com.paulotech.Desafio.backend.IFood.Api.domain.categories.exceptions.CategoryNotFoundException;
 import com.paulotech.Desafio.backend.IFood.Api.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -18,4 +21,25 @@ public class CategoryService {
         this.repository.save(newCategory);
         return newCategory;
     }
+
+    public List<Category> getAll(){
+        return this.repository.findAll();
+    }
+
+    public Category update(String id, CategoryDTO categoryData){
+        Category category = this.repository.findById(id)
+                .orElseThrow(CategoryNotFoundException::new);
+        if(!categoryData.title().isEmpty()) category.setTitle(categoryData.title());
+        if(!categoryData.description().isEmpty()) category.setDescription(categoryData.description());
+
+        this.repository.save(category);
+        return category;
+    }
+
+    public void delete(String id){
+        Category category = this.repository.findById(id)
+                .orElseThrow(CategoryNotFoundException::new);
+        this.repository.delete(category);
+    }
+
 }
